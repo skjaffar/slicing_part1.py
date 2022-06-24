05eb4b35334a1a370f4823cfef3728cd67bcba47
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-23 22:51:05 -0700

    Add files via upload

---
+++ slicing_part.py.py
@@ -0,0 +1,37 @@
+s='computer'
+# print(s[:7]) # compute
+# print(s[:9]) # computer
+# print(s[:6]) # comput
+# print(s[:3]) # com
+# print(s[:5]) # compu
+
+# print(s[2:3]) # m
+# print(s[2:8]) # mputer
+# print(s[5:2]) # blank line
+# print(s[3:8]) # puter
+
+# print(s[:-5])  # com
+# print(s[:-7])  # c
+# print(s[:-6])  # co
+# print(s[:-1])  # compute
+# print(s[:-3]) # compu
+
+# print(s[-8:-6])  # co
+# print(s[-7:-1])  # compute
+# print(s[-3:-1])  # te
+
+# print(s[::5]) # ct
+# print(s[::8]) # c
+# print(s[::-6]) # ro
+# print(s[::-3]) # ruo
+
+# print(s[3::7]) # p
+# print(s[2::4]) # me
+# print(s[2:7])  # mpute
+# print(s[-2::3]) # e
+
+# print(s[:8:5])  # ct
+# print(s[2:3:8]) # m
+# print(s[: :-2]) # rtpo
+# print(s[: :-5]) # rm
+# print(s[::-1]) # retupmoc
